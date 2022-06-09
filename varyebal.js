7f09525a86e45add0f029d20bd2f38537b3d0f83
Hardik <hardikchauhan442@gmail.com>
2022-06-09 19:43:33 +0530

    9-6-22

---
+++ varyebal.js
@@ -0,0 +1,19 @@
+
+// var a = 10;
+// var b = 10;
+// var c = -1;
+// true =1
+// false=0
+// console.log(a > b);
+// console.log(a < b);
+// console.log(typeof true);
+// console.log(true + true);
+// console.log(true * true);
+// console.log(true * false);
+// console.log(true / false);
+// console.log(true / false);
+// console.log(true + true + true * false);
+// var a = 10;
+// var b = 20;
+
+// console.log(a * "b");
